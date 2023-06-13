4c1d2911eb85c18df9689c4c97f0681ebf96047c
Taka <soutakaoka92@gmail.com>
2023-06-13 23:04:12 +0900

    ボタンを表示する処理を追加

--- app/src/main/java/com/soutaka/hitandbyte/presentation/home/HomeScreen.kt
+++ app/src/main/java/com/soutaka/hitandbyte/presentation/home/HomeScreen.kt
@@ -1,2 +1,18 @@
 package com.soutaka.hitandbyte.presentation.home
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScrren() {
+    Column() {
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Play Game")
+        }
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "対戦記録")
+        }
+    }
+}
